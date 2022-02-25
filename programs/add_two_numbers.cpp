de29422cbba9465c096feb9f2020026283e85f67
MKrishan21 <99832785+MKrishan21@users.noreply.github.com>
2022-02-25 23:10:05 +0530

    program for adding two numbers

---
+++ programs/add_two_numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    cout << "enter first number\n";
+    cin >> a;
+    cout << "enter second number\n";
+    cin >> b;
+    cout << "sum of two numbers is " << a + b << endl;
+
+    return 0;
+}
