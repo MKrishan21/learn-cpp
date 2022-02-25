414ec99a0ab7a3c11361e9043e845c3fa1b7ea8a
MKrishan21 <99832785+MKrishan21@users.noreply.github.com>
2022-02-25 22:48:55 +0530

    hello world in cpp

---
+++ basics/hello_world.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+//#include is a way of including a user-defined file in the program
+
+using namespace std;
+// It is known that “std” (abbreviation for the standard) is a namespace whose members are used in the program
+
+int main()
+{
+    /*'int main' means that our function needs
+    to return some integer at the end of the execution
+     and we do so by returning 0 at the end of the program.*/
+
+    cout << "Hello world";
+    return 0;
+}
