3b33ed3c0808a443519887157afa1faa467191e1
MKrishan21 <99832785+MKrishan21@users.noreply.github.com>
2022-03-20 22:24:40 +0530

    fix: nth element from start

--- programs/array/03_print_nth_element_from_start.cpp
+++ programs/array/03_print_nth_element_from_start.cpp
@@ -3,10 +3,27 @@ using namespace std;
 
 int main()
 {
-    int marks[] = {10, 11, 12, 13, 14, 15, 16, 17};
-    // program to print length of array
-    int n = sizeof(marks) / sizeof(marks[0]);
-    // program to print Nth term
-    cout << n << endl;
+    int marks[] = {10, 11, 22, 13, 77, 15, 16, 17};
+
+    int no_of_elements = sizeof(marks) / sizeof(marks[0]);
+
+    cout << "Enter the value of n for printing the nth element in the array." << endl;
+
+    int n;
+
+    cin >> n;
+
+    if (no_of_elements < n)
+    {
+        cout << "Invalid input." << endl;
+    }
+    else if (n == 0 || n < 0)
+    {
+        cout << "Please enter a positive number greater than 0." << endl;
+    }
+    else
+    {
+        cout << n << "th element in array is " << marks[n - 1];
+    }
     return 0;
 }
