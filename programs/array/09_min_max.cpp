c1c1f295be27a5831d891e245a91cdd436c521db
MKrishan21 <99832785+MKrishan21@users.noreply.github.com>
2022-03-31 23:12:56 +0530

    find minimum and maximum in an array

--- programs/array/09_min_max.cpp
+++ programs/array/09_min_max.cpp
@@ -3,25 +3,31 @@ using namespace std;
 
 int main()
 {
-    int marks[] = {32, 26, 63, 79, 77, 15, 53, 17};
-    int n = sizeof(marks) / sizeof(marks[0]);
-    // int a = marks[0];
-    cout << "this is the max number in the array " << endl;
-    for (int i = 0; i < n; i++)
-    {
-        if (marks[0] < marks[i])
-            marks[0] = marks[i];
-    }
-    cout << marks[0] << endl;
+    int arr[] = {32, 26, 63, 79, 77, 15, 53, 17};
+    int n = sizeof(arr) / sizeof(arr[0]);
 
-    cout << "this is the min number in the array " << endl;
+    int max = arr[0];
 
-    for (int j = 0; j < n; j++)
+    for (int i = 1; i < n; i++)
     {
-        if (marks[0] > marks[j])
-            marks[0] = marks[j];
+        if (max < arr[i])
+        {
+            max = arr[i];
+        }
     }
-    cout << marks[0] << endl;
+
+    cout << "Max element in array: " << max << endl;
+
+    int min = arr[0];
+
+    for (int i = 1; i < n; i++)
+    {
+        if (min > arr[i])
+        {
+            min = arr[i];
+        }
+    }
+    cout << "Min element in array: " << min << endl;
 
     return 0;
 }
